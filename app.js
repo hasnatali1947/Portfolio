6700739fd455746c35e4774809de73606020b07b
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-06 21:52:12 +0500

    remove es lint error

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const menuLinks = document.getElementById('menuChild');
 const hamburger = document.getElementById('menuIcon');
-const iconCancel = document.getElementById('cancel-icon');
 const maincontent = document.getElementById('mobmain');
 const logo = document.getElementById('logo');
 
-function OpenMenu () {
-  menuLinks.style.display = "flex";
-  menuLinks.style.visibility = "visible";
-  hamburger.style.display = "none";
-  maincontent.classList.add("blur");
-  logo.classList.add("blur");
-};
+const openMenu = document.querySelector('.openMenu');
 
-function CloseMenu () {
-  menuLinks.style.display = "none";
-  menuLinks.style.visibility= "hidden";
-  hamburger.style.display = "flex";
-  maincontent.classList.remove("blur");
-  logo.classList.remove("blur");
-};
+openMenu.addEventListener('click', (() => {
+  menuLinks.style.display = 'flex';
+  menuLinks.style.visibility = 'visible';
+  hamburger.style.display = 'none';
+  maincontent.classList.add('blur');
+  logo.classList.add('blur');
+}));
+
+const closeMenu = document.querySelector('.closeMenu');
+
+closeMenu.addEventListener('click', (() => {
+  menuLinks.style.display = 'none';
+  menuLinks.style.visibility = 'hidden';
+  hamburger.style.display = 'flex';
+  maincontent.classList.remove('blur');
+  logo.classList.remove('blur');
+}));
