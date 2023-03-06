55206c381ee6c744444e7b1826b2e6deca167a22
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-06 20:26:52 +0500

    remove ES lint

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let menuLinks = document.getElementById('menuChild')
-let hamburger = document.getElementById('menuIcon')
-let iconCancel = document.getElementById('cancel-icon')
-let maincontent = document.getElementById('mobmain')
-let logo = document.getElementById('logo')
+const menuLinks = document.getElementById('menuChild');
+const hamburger = document.getElementById('menuIcon');
+const iconCancel = document.getElementById('cancel-icon');
+const maincontent = document.getElementById('mobmain');
+const logo = document.getElementById('logo');
 
-function OpenMenu (){
-    menuLinks.style.display = "flex"
-    menuLinks.style.visibility = "visible"
-    hamburger.style.display = "none"
-    maincontent.classList.add("blur")
-    logo.classList.add("blur")
-}
+function OpenMenu () {
+  menuLinks.style.display = "flex";
+  menuLinks.style.visibility = "visible";
+  hamburger.style.display = "none";
+  maincontent.classList.add("blur");
+  logo.classList.add("blur");
+};
 
-function CloseMenu (){
-    menuLinks.style.display = "none"
-    menuLinks.style.visibility= "hidden"
-    hamburger.style.display = "flex"
-    maincontent.classList.remove("blur")
-    logo.classList.remove("blur")
-}
+function CloseMenu () {
+  menuLinks.style.display = "none";
+  menuLinks.style.visibility= "hidden";
+  hamburger.style.display = "flex";
+  maincontent.classList.remove("blur");
+  logo.classList.remove("blur");
+};
