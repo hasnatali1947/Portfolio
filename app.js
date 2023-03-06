68d39e31f13193a05af170f28250566685016ad1
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-06 22:33:05 +0500

    added gitignore file

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const hamburger = document.getElementById('menuIcon');
 const maincontent = document.getElementById('mobmain');
 const logo = document.getElementById('logo');
 
-const openMenu = document.querySelector('.openMenu')
+const openMenu = document.querySelector('.openMenu');
 
-openMenu.addEventListener('click' , (()=>{
+openMenu.addEventListener('click', (() => {
   menuLinks.style.display = 'flex';
   menuLinks.style.visibility = 'visible';
   hamburger.style.display = 'none';
   maincontent.classList.add('blur');
   logo.classList.add('blur');
-}))
+}));
 
-const closeMenu = document.querySelector('.closeMenu')
+const closeMenu = document.querySelector('.closeMenu');
 
-closeMenu.addEventListener('click' , (()=>{
+closeMenu.addEventListener('click', (() => {
   menuLinks.style.display = 'none';
   menuLinks.style.visibility = 'hidden';
   hamburger.style.display = 'flex';
   maincontent.classList.remove('blur');
   logo.classList.remove('blur');
-}))
+}));
