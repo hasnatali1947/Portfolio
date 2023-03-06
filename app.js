e71d4acde50b3fe26799010b36ed0bf8300e06f2
rosemutai <chepngetichrose2030@gmail.com>
2023-03-06 17:51:06 +0300

    Complete mobile menu using javascript

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-let menuIcon = document.getElementById("menuIcon")
-let menuChild = document.getElementById("menuChild")
+let menuLinks = document.getElementById('menuChild')
+let hamburger = document.getElementById('menuIcon')
+let iconCancel = document.getElementById('cancel-icon')
+let maincontent = document.getElementById('mobmain')
+let logo = document.getElementById('logo')
 
+function OpenMenu (){
+    menuLinks.style.display = "flex"
+    menuLinks.style.visibility = "visible"
+    hamburger.style.display = "none"
+    maincontent.classList.add("blur")
+    logo.classList.add("blur")
+}
+
+function CloseMenu (){
+    menuLinks.style.display = "none"
+    menuLinks.style.visibility= "hidden"
+    hamburger.style.display = "flex"
+    maincontent.classList.remove("blur")
+    logo.classList.remove("blur")
+}
