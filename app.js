bf5e55b2cb81060c89f2e1b9f75dee17f15acb61
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-07 15:26:42 +0500

    some changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const menuLinks = document.getElementById('menuChild');
 const hamburger = document.getElementById('menuIcon');
 const maincontent = document.getElementById('mobmain');
 const logo = document.getElementById('logo');
-
+const navlink = document.getElementsByClassName('a') 
 const openMenu = document.querySelector('.openMenu');
 
 openMenu.addEventListener('click', (() => {
