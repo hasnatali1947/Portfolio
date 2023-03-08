e287149608b51aeb0e8dc9dc54a6f7e4f58cae0e
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-08 18:20:47 +0500

    added js in mobile and desktop version on seeproject button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const menuLinks = document.getElementById('menuChild');
 const hamburger = document.getElementById('menuIcon');
 const maincontent = document.getElementById('mobmain');
@@ -5,6 +6,11 @@ const logo = document.getElementById('logo');
 const openMenu = document.querySelector('.openMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const navlinks = document.querySelectorAll('.a');
+const seeproject = document.querySelectorAll('.Button-SeeProject');
+const desktopMenu = document.querySelector('.desktopMenu');
+const modal = document.querySelector('#modal');
+const projectImage = document.querySelector('.idcard-modal');
+const projectTitle = document.querySelector('.modal-tonic');
 
 openMenu.addEventListener('click', (() => {
   menuLinks.style.display = 'flex';
@@ -31,4 +37,70 @@ for (let i = 0; i < navlinks.length; i += 1) {
     hamburger.classList.remove('blur');
     hamburger.style.display = 'flex';
   });
+}
+
+/// /////////cards/////////
+
+const storeData = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: 'Snapshoot Portfolio.png',
+    technologies: ['html', 'css', 'javaScript'],
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: 'desktop-2.png',
+    technologies: ['html', 'css', 'javaScript'],
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: 'desktop-3.png',
+    technologies: ['html', 'css', 'javaScript'],
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: 'desktop-4.png',
+    technologies: ['html', 'css', 'javaScript'],
+  },
+];
+
+function disappearButton() {
+  modal.style.display = 'none';
+  maincontent.classList.remove('blur');
+}
+
+function SeeProject() {
+  modal.style.display = 'flex';
+  modal.style.visibility = 'visible';
+  maincontent.classList.add('blur');
+  projectImage.src = storeData[0].imagesrc;
+  projectTitle.innerHTML = storeData[0].name;
+}
+
+function Seeproject2() {
+  modal.style.display = 'flex';
+  modal.style.visibility = 'visible';
+  maincontent.classList.add('blur');
+  projectImage.src = storeData[1].imagesrc;
+  projectTitle.innerHTML = storeData[1].name;
+}
+
+function Seeproject3() {
+  modal.style.display = 'flex';
+  modal.style.visibility = 'visible';
+  maincontent.classList.add('blur');
+  projectImage.src = storeData[2].imagesrc;
+  projectTitle.innerHTML = storeData[2].name;
+}
+
+function Seeproject4() {
+  modal.style.display = 'flex';
+  modal.style.visibility = 'visible';
+  maincontent.classList.add('blur');
+  projectImage.src = storeData[3].imagesrc;
+  projectTitle.innerHTML = storeData[3].name;
 }
